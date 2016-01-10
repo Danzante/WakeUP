440caef1dadfc4af32467c97532e0eede63d884f
Danzante <tim-95@narod.ru>
2016-01-10 19:12:27 +0300

    Main
    Moving and Shooting

---
+++ Wake Up/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController: MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    float speed = 6.0f;
+    float jumpSpeed = 8.0f;
+    float gravity = 20.0f;
+    float rotSpeed = 900;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime, 0);
+
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
+                                    Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Wake Up/Assets/ShootControll.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootControll : MonoBehaviour
+{
+
+    float rotationY;
+    private GameObject Head;
+    private GameObject Body;
+
+    // Use this for initialization
+    void Start()
+    {
+        Body = transform.parent.gameObject;
+        Head = GameObject.Find("/Player/Head");
+    }
+
+    void Shoot()
+    {
+        Ray myray = new Ray(transform.position, transform.forward);
+        RaycastHit help;
+        if (Physics.Raycast(myray, out help))
+        {
+            Debug.Log(help.point);
+            help.collider.gameObject.GetComponent<HPcounter>().GetStrike_Heall(10);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationY = Input.GetAxis("Mouse Y") * 10F;
+        if (((Mathf.Abs(Vector3.Angle(Head.transform.forward, Body.transform.forward) - rotationY) < 50) && (Vector3.Angle(Head.transform.forward, Body.transform.up) > 90)) || ((Mathf.Abs(Vector3.Angle(Head.transform.forward, Body.transform.forward) + rotationY) < 70) && (Vector3.Angle(Head.transform.forward, Body.transform.up) <= 90)))
+            Head.transform.Rotate(new Vector3(-rotationY, 0, 0));
+        
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+}
