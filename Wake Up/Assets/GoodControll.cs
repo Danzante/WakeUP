d4e8e501ed3b61598246e5334d293bea777da2f9
Danzante <tim-95@narod.ru>
2016-01-22 02:28:44 +0300

    Upgraded AI
    not detecting aims yet...

--- Wake Up/Assets/GoodControll.cs
+++ Wake Up/Assets/GoodControll.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class GoodControll : MonoBehaviour {
 
@@ -10,7 +11,12 @@ public class GoodControll : MonoBehaviour {
     {
         Game = GameObject.Find("/Game");
         GameObject.Find("/Player").GetComponent<HPcounter>().typeOb = 1;
+        aimX = transform.position.x;
+        aimZ = transform.position.z;
+        aimRot = transform.rotation.y;
     }
+
+    System.Random r = new System.Random();
 
     float speed = 6.0f;
     float jumpSpeed = 8.0f;
@@ -19,24 +25,57 @@ public class GoodControll : MonoBehaviour {
 
     private Vector3 moveDirection = Vector3.zero;
 
+    float aimX, aimZ, aimRot;
+
+    struct cord  {
+        public float x, z, rot;
+    }
+
+    Stack stack = new Stack();
+    int memory = 0;
+
     void Detect()
     {
-
+        memory += 1;
+        cord a;
+        a.x = aimX;
+        a.z = aimZ;
+        a.rot = aimRot;
+        stack.Push(a);
+        for(int i = 4; i < memory; i++)
+        {
+            if(r.Next(0, 6) < 2)
+            {
+                memory = i;
+            }
+        }
     }
 
     float CountX()
     {
-        return 0;
+        if(aimX == transform.position.x && aimZ == transform.position.z)
+        {
+            Detect();
+        }
+        return aimX - transform.position.x;
     }
 
     float CountRotX()
     {
-        return 0;
+        if (aimX == transform.position.x && aimZ == transform.position.z)
+        {
+            Detect();
+        }
+        return aimRot - transform.rotation.y;
     }
 
     float CountZ()
     {
-        return 0;
+        if (aimX == transform.position.x && aimZ == transform.position.z)
+        {
+            Detect();
+        }
+        return aimZ - transform.position.Z;
     }
 
     void Play()
